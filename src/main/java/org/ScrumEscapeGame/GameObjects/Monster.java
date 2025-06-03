8ab82ed7234b98e86a62831cce91678baec77c47
S3mCost3r <semcoster02@gmail.com>
2025-06-03 12:20:12 +0200

    monster klasse begin

--- src/main/java/org/ScrumEscapeGame/GameObjects/Monster.java
+++ src/main/java/org/ScrumEscapeGame/GameObjects/Monster.java
@@ -1,32 +1,25 @@
 package org.ScrumEscapeGame.GameObjects;
 
-public class Monster {
+import org.ScrumEscapeGame.AAEvents.EventPublisher;
+import org.ScrumEscapeGame.AAEvents.GameEvent;
+import org.ScrumEscapeGame.AAGame.GameContext;
+
+public abstract class Monster {
     private String name;
     private String description;
     private boolean alive;
-    private boolean boss;
-    private final int    requiredCorrect; // hoeveel juiste antwoorden bij boss
-    private int correctCount = 0; // teller hoeveel boss vragen je goed heb
-    private int roomId;
+    private EventPublisher<GameEvent> publisher;
+    private GameContext context;
 
-
-    //dit is een NORMAAL monster
-    public Monster(String name, String description) {
-        this(name, description, false, 1);
+    public Monster(String name, String description, boolean alive, EventPublisher<GameEvent> publisher, GameContext context) {
+        this.name = name;
+        this.description = description;
+        this.alive = false;
+        this.publisher = publisher;
+        this.context = context;
     }
 
-
-    // dit is een BOSS MONSTER je moet zoveel goed hebben als de waarde van requiredCorrect
-    public Monster(String name, String description, int requiredCorrect) {
-        this(name, description, true, requiredCorrect);
+    public boolean Dying() {
+        return false;
     }
-
-    private Monster(String name, String description, boolean boss, int requiredCorrect) {
-        this.name            = name;
-        this.description     = description;
-        this.boss            = boss;
-        this.requiredCorrect = requiredCorrect;
-        this.alive           = false;
-    }
-
 }
